e8c19f178bf37c0559a8ff7d2e1406f51c7b63ac
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-09-08 12:54:00 +0900

    update master
    トレーニングとテスト用データの準備。

--- HappyNews_TextClassificationModel.playground/Contents.swift
+++ HappyNews_TextClassificationModel.playground/Contents.swift
@@ -1,8 +1,24 @@
 import CreateML
 import Cocoa
 
-//テキスト分類データの準備
+//感情分類ファイルの呼び出し(準備)
 guard let trainingDataFileURL = Bundle.main.url(forResource: "sentiment_analysis_training", withExtension: "json"),
       let testingDataFileURL  = Bundle.main.url(forResource: "sentiment_analysis_test", withExtension: "json") else {
     fatalError("Error! Couldn't load resource files")
+}
+
+//トレーニングとテスト用データの準備
+do {
+    let trainingDataTable = try MLDataTable(contentsOf: trainingDataFileURL)
+    let testingDataTable  = try MLDataTable(contentsOf: testingDataFileURL)
+    
+    let stats = """
+    ===================================================
+    Entries used of training: \(trainingDataTable.size)
+    Entries used of testing : \(testingDataTable.size)
+"""
+    print(stats)
+    
+} catch {
+    print(error.localizedDescription)
 }
