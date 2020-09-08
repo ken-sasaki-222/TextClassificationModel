156cc57ba28ed45f50d3a7a56a81218e48478ef8
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-09-08 11:04:56 +0900

    update master
    感情分類ファイルの呼び出し

--- HappyNews_TextClassificationModel.playground/Contents.swift
+++ HappyNews_TextClassificationModel.playground/Contents.swift
@@ -1,3 +1,8 @@
+import CreateML
 import Cocoa
 
-var str = "Hello, playground"
+//テキスト分類データの準備
+guard let trainingDataFileURL = Bundle.main.url(forResource: "sentiment_analysis_training", withExtension: "json"),
+      let testingDataFileURL  = Bundle.main.url(forResource: "sentiment_analysis_test", withExtension: "json") else {
+    fatalError("Error! Couldn't load resource files")
+}
