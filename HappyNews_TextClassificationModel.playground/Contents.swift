3387932c5cfdc972d443e09b39150730806dd72d
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-09-10 23:28:01 +0900

    update master
    分類器の精度の評価を実装

--- HappyNews_TextClassificationModel.playground/Contents.swift
+++ HappyNews_TextClassificationModel.playground/Contents.swift
@@ -22,6 +22,23 @@ do {
     //テキスト分類の作成とトレーニング
     let sentimentClassifier = try MLTextClassifier(trainingData: trainingDataTable, textColumn: "text", labelColumn: "label")
     
+    //分類器の精度の評価
+    let evaluationMetrics = sentimentClassifier.evaluation(on: testingDataTable)
+    
+    let trainingAccuracy   = (1.0 - sentimentClassifier.trainingMetrics.classificationError) * 100
+    let validationAccuracy = (1.0 - sentimentClassifier.validationMetrics.classificationError) * 100
+    let evaluationAccuracy = (1.0 - evaluationMetrics.classificationError) * 100
+
+    
+    let message = """
+    ===================================================
+    Training   accuracy: \(trainingAccuracy)
+    Varidation accuracy: \(validationAccuracy)
+    Evaluation accuracy: \(evaluationAccuracy)
+"""
+    
+    print(message)
+    
 } catch {
     print(error.localizedDescription)
 }
